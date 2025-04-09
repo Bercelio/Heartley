9fc784980d103d3cdfe6669190f407a522a274f9
Bercelio <bercelio@gmail.com>
2025-04-09 15:58:22 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import numpy as np
 import joblib
 import os
 import matplotlib.pyplot as plt
-import gdown  # Usamos gdown en lugar de requests
+import gdown
 
 app = Flask(__name__)
 
@@ -103,4 +103,5 @@ def resultado():
         return f"Ocurrió un error: {str(e)}"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
